78ce52814b98ac981474246f43a2e03e2711745e
Anita <a.nita@hotmail.co.uk>
2016-04-23 10:54:23 +0100

    refactore functional component to class component in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import API_KEY from './components/keys';
 
-// Create a new component , which produece some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+      this.setState({ videos: data })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated html and put this in DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
